f7fb367fc091d81ce78470bb14ebc38e55ddb156
timothylidede <75303411+timothylidede@users.noreply.github.com>
2022-01-02 23:07:50 +0300

    start activity class initializing widgets

--- app/src/main/java/com/savala/keynyozi/StartActivity.java
+++ app/src/main/java/com/savala/keynyozi/StartActivity.java
@@ -6,8 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
+
+    //widgets
+    private Button mLogin, mSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +20,8 @@ public class StartActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_start);
+
+        mLogin = (Button) findViewById(R.id.login_button);
+        mSignUp = (Button) findViewById(R.id.signup_button);
     }
 }
