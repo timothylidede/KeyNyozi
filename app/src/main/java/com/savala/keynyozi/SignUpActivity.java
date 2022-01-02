31af38c159d9d06ec8cea9cd5394c02494cfab72
timothylidede <75303411+timothylidede@users.noreply.github.com>
2022-01-02 23:33:27 +0300

    adding design to signup activity layout

--- app/src/main/java/com/savala/keynyozi/SignUpActivity.java
+++ app/src/main/java/com/savala/keynyozi/SignUpActivity.java
@@ -3,12 +3,16 @@ package com.savala.keynyozi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(getWindow().FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_sign_up);
     }
 }
